93fc5064149de258498ede5aa38f53719c5fea0c
Fan <yfctw555@gmail.com>
2022-05-09 20:39:21 +0800

    strtotime()函式的應用的應用

--- datepra00.php
+++ datepra00.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <h2>日期/時間練習</h2>
+    <h2>日期/時間練習 格式化 format</h2>
     <h4>未設定timezone</h4>
     <?php
     echo date("Y-m-d H:i:s");
@@ -21,6 +21,28 @@
     echo date("Y-m-d H:i:s");
     echo "<br>";
     echo date("Y 年 n 月 d 日 H 點 i 分 s 秒");
+    ?>
+
+    <h2>strtotime()</h2>
+    <?php
+    // 時間日期字串格式 yyyy-mm-dd hh:mm:ss
+    $time = "2021-10-01 10:12:30";
+    $second = strtotime($time);
+    echo $time;
+    echo "<br>";
+    echo $second;
+    // 之後放到date裡去做日期時間換算的基礎
+    echo "<br>";
+    echo date("西元 Y 年 m 月 d 日 l", $second);
+    echo "<br>";
+    $secondPlus = strtotime("+2 days", $second);
+    echo date("西元 Y 年 m 月 d 日 l", $secondPlus);
+    echo "<br>";
+    $secondPlus = strtotime("+1 month", $second);
+    echo date("西元 Y 年 m 月 d 日 l", $secondPlus);
+    echo "<br>";
+    $secondPlus = strtotime("-1 week", $second);
+    echo date("西元 Y 年 m 月 d 日 l", $secondPlus);
     ?>
 
 </body>
