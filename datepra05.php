5f1b80155f4f021c0d7148cb4b72d7097114fe31
Fan <yfctw555@gmail.com>
2022-05-12 19:56:12 +0800

    完成單月月曆，加class和判斷今日的功能今日的功能

--- datepra05.php
+++ datepra05.php
@@ -7,20 +7,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>線上月曆</title>
     <style>
-        table{
+        table {
             border-collapse: collapse;
         }
-        table td{
+
+        table td {
             padding: 5px;
             text-align: center;
             border: 1px solid #aaa;
+        }
+        .weekday{
+            background-color: beige;
+        }
+        .weekend{
+            background-color: lightpink;
+        }
+        .today{
+            background-color: lightblue;
         }
     </style>
 </head>
 
 <body>
     <?php
-    $month = 4;
+    $month = 5;
     ?>
     <table>
         <tr>
@@ -33,33 +43,68 @@
             <td>Sat</td>
         </tr>
         <?php
-        $firstDay=date("Y/").$month."/1";
-        $firstWeekDay=date("w",strtotime($firstDay));
-        $monthDays=date("t",strtotime($firstDay));
+        $firstDay = date("Y/") . $month . "/1";
+        $firstWeekDay = date("w", strtotime($firstDay));
+        $monthDays = date("t", strtotime($firstDay));
         // t 是識別指定月份有幾天的格式字串
-        $lastDay=date("Y/").$month."/".$monthDays;
+        $lastDay = date("Y/") . $month . "/" . $monthDays;
+        $today=date("Y-m-d");
 
-        echo $month." 月"."共有 ".$monthDays." 天";
+        echo $month . " 月" . "共有 " . $monthDays . " 天";
         echo "<br>";
-        echo "第一天是 ".$firstDay;
+        echo "第一天是 " . $firstDay;
         echo "<br>";
-        echo "第一天是星期 ".$firstWeekDay;
+        echo "第一天是星期 " . $firstWeekDay;
         echo "<br>";
-        echo "最後一天是 ".$lastDay;
+        echo "最後一天是 " . $lastDay;
 
+
+        // for ($i = 0; $i < 6; $i++) {
+        //     echo "<tr>";
+        //     for ($j = 0; $j < 7; $j++) {
+        //         echo "<td>";
+        //         if($i==0 && $j==$firstWeekDay){
+        //             echo "1"; 
+        //          用來找出第一週第一天所在位置
+        //         }else if($i==0 && $j<$firstWeekDay){
+        //             echo "";
+        //         }else{
+        //         echo $i*7+($j+1);
+        //     }
+        //     echo "</td>";
+        //     }
+        //     echo "</tr>";
+        // }
 
         for ($i = 0; $i < 6; $i++) {
             echo "<tr>";
             for ($j = 0; $j < 7; $j++) {
-                echo "<td>";
-                if($i==0 && $j==$firstWeekDay){
-                    echo "第一天"; 
-                }else if($i==0 && $j<$firstWeekDay){
-                    echo "";
+                $d = $i * 7 + ($j + 1) - $firstWeekDay - 1;
+                // $d用來確認日期是在格子中的第幾天
+
+                if ($d >= 0 && $d < $monthDays) {
+                    // $date=date("Y-m-d",strtotime("+$d days",strtotime($firstDay)));
+                    $fs = strtotime($firstDay);
+                    $shiftDay = strtotime("+$d days", $fs);
+                    $date = date("j", $shiftDay);
+
+                    $w = date("w", $shiftDay);
+                    $chktoday="";
+                    if(date("Y-m-d",$shiftDay)==$today){
+                        $chktoday='today';
+                    }
+                    if ($w == 0 || $w == 6) {
+
+                        echo "<td class='weekend $chktoday'>";
+                    } else {
+                        echo "<td class='weekday $chktoday'>";
+                    }
+
+                    echo $date;
+                    echo "</td>";
                 }else{
-                echo $i*7+($j+1);
-            }
-            echo "</td>";
+                    echo "<td></td>";
+                }
             }
             echo "</tr>";
         }
